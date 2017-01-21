2c323315c625ea5420c4f82f543104d4d8af0501
Arta <martin.fencl@gmail.com>
2017-01-21 13:58:49 -0600

    Stub posts RESTful controller actions and views.

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,7 +1,47 @@
 class PostsController < ApplicationController
+  before_action :set_post, only: [:show, :edit, :update, :destroy]
 
   # GET /posts
   def index
-    
+
   end
+
+  # GET /posts/new
+  def new
+
+  end
+
+  # POST /posts
+  def create
+
+  end
+
+  # GET /posts/:id
+  def show
+
+  end
+
+  # GET /posts/:id/edit
+  def edit
+
+  end
+
+  # PATCH|PUT /posts/:id
+  def update
+
+  end
+
+  # DELETE /posts/:id
+  def destroy
+
+  end
+
+  private
+    def post_params
+
+    end
+
+    def set_post
+
+    end
 end
