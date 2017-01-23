f3ab6cc5d826836e07cea5e9dbb746d8374e2ce4
Arta <martin.fencl@gmail.com>
2017-01-22 23:04:05 -0600

    User can create a Post Comment from /posts/show page.

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,2 +1,18 @@
 class CommentsController < ApplicationController
+
+  # POST /posts/:post_id/commets
+  #   html:   <form action='/posts/1/comments' ..>
+  #             <input name='comment[content]' ..>
+  #             ...
+  #   rails:  =form_for [@post, @post.comments.new]
+  #             =f.input :content
+  #             ...
+  #   router: post '/posts/:post_id/comments', to: 'comments#create'
+  def create
+    post = Post.find params[:post_id]
+    post.comments.create( params[:comment].permit( :content ).merge( user: current_user) )
+    redirect_to post, notice:'Comment posted.'
+  end
+  # No view. Explicitly redirect_to a resource.
+
 end
