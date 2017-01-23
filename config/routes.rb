87b417fe0a609149c211d5eccba935155ba77c2b
Arta <martin.fencl@gmail.com>
2017-01-23 13:03:49 -0600

    Draw Post Voting resources routes.

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,10 @@ Rails.application.routes.draw do
   devise_for :users
   resources :posts do
     resources :comments, shallow: true, only: [:create, :destroy]
+    member do
+      put 'like',    to: 'posts#upvote'
+      put 'dislike', to: 'posts#downvote'
+    end
   end
 
   root 'posts#index'
