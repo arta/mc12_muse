b03967618c005e81f73e8b9ff371d27604e6a9af
Arta <martin.fencl@gmail.com>
2017-01-23 12:36:26 -0600

    Post acts_as_votable
    Works in $rails console.

--- app/models/post.rb
+++ app/models/post.rb
@@ -3,4 +3,5 @@ class Post < ApplicationRecord
   has_many :comments, dependent: :destroy
   has_attached_file :image, styles: { medium: "700x500#", small: "350x250#" }
   validates_attachment_content_type :image, content_type: /\Aimage\/.*\z/
+  acts_as_votable
 end
